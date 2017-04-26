77450a419efe4ff54f0e64af08f547154c49f0df
Ryan <ryanjonesirl@gmail.com>
2017-04-26 12:11:30 -0700

    add the functionality for associating reviews to a single rental (one-many), when you make multiple calls to firebase, there is a delay, and the multiple calss have to be structured in a certain way to battle the async

--- app/components/new-review.js
+++ app/components/new-review.js
@@ -3,17 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   addNewReview: false,
   actions: {
-    saveReview() {
-      var params = {
-        author: this.get('author'),
-        rating: this.get('rating'),
-        content: this.get('content')
-      };
-      this.set('addNewReview', false);
-      this.sendAction('saveReview', params);
-    },
     reviewFormShow() {
       this.set('addNewReview', true);
-    }
+    },
+    saveReview() {
+     var params = {
+       author: this.get('author'),
+       rating: this.get('rating'),
+       content: this.get('content'),
+       rental: this.get('rental')
+     };
+     this.set('addNewReview', false);
+     this.sendAction('saveReview', params);
+   }
   }
 });
--- app/routes/rental.js
+++ app/routes/rental.js
@@ -15,7 +15,27 @@ export default Ember.Route.extend({
       this.transitionTo('index');
     },
     destroyRental(rental) {
-      rental.destroyRecord();
+      var review_deletions = rental.get('reviews').map(function(review) {
+        return review.destroyRecord();
+      });
+      Ember.RSVP.all(review_deletions).then(function() {
+        return rental.destroyRecord();
+      });
+      this.transitionTo('index');
+    },
+    // It looks like we're saving redundantly, but really we are saving the review to the review section of our json document then we have to save it within that specific rental.
+    // there is an issue with the response time it takes to go from ember -> firebase, so we need to use promises to prevent the data from being stored improperly
+    saveReview(params) {
+      var newReview = this.store.createRecord('review', params); // we store the review in the db (in review section)
+      var rental = params.rental; // take rental passed in params, save as variable 'rental'
+      rental.get('reviews').addObject(newReview); // we add our newReview into that specific rentals review list
+      newReview.save().then(function() { // we save the review into that list, then save the rental
+        return rental.save(); // Question: Why are we saving the rental here? Is it not already saved?
+      });
+      this.transitionTo('rental', rental); // direct back to the rental route
+    },
+    destroyReview(review) {
+      review.destroyRecord();
       this.transitionTo('index');
     }
   }
